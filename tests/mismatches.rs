40d2a3aa6d94398b5ce32cd680309c1a4b4c9cdd
Sean Leffler <sean@errno.com>
2020-02-12 21:46:55 -0800

    fix warnings in test and version bump serde_json

--- tests/mismatches.rs
+++ tests/mismatches.rs
@@ -3,27 +3,35 @@ use std::fs;
 #[test]
 fn mismatches() {
     let mut meta_egress = egress::egress!();
-    let mut meta_artifact = meta_egress.artifact("mismatches");
+    let meta_artifact = meta_egress.artifact("mismatches");
 
     // Refresh so that we're starting from a clean slate
     fs::remove_file("tests/mismatches/egress/artifacts/mismatches/test.json").unwrap();
 
     let reference_mismatches = {
         let mut egress = egress::egress!("tests/mismatches");
-        let mut artifact = egress.artifact("test");
-        artifact.insert_serialize("fruits", &vec!["apples", "bananas", "oranges"]);
+        let artifact = egress.artifact("test");
+        artifact
+            .insert_serialize("fruits", &vec!["apples", "bananas", "oranges"])
+            .unwrap();
         egress.close().unwrap()
     };
 
     let new_mismatches = {
         let mut egress = egress::egress!("tests/mismatches");
-        let mut artifact = egress.artifact("test");
-        artifact.insert_serialize("fruits", &vec!["apples", "pears", "oranges"]);
+        let artifact = egress.artifact("test");
+        artifact
+            .insert_serialize("fruits", &vec!["apples", "pears", "oranges"])
+            .unwrap();
         egress.close().unwrap()
     };
 
-    meta_artifact.insert_serialize("reference_mismatches", &reference_mismatches);
-    meta_artifact.insert_serialize("new_mismatches", &new_mismatches);
+    meta_artifact
+        .insert_serialize("reference_mismatches", &reference_mismatches)
+        .unwrap();
+    meta_artifact
+        .insert_serialize("new_mismatches", &new_mismatches)
+        .unwrap();
 
     meta_egress.close().unwrap().assert_unregressed();
 }
